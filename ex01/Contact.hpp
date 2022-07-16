eb30a3977c5d5f867003ac0617782242876c9164
sohan <sohan@student.42seoul.kr>
2022-07-16 19:44:17 +0900

    [add] contact and phonebook class file

---
+++ ex01/Contact.hpp
@@ -0,0 +1,19 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <string>
+
+class	Contact
+{
+	private:
+		std::string firstName;
+		std::string lastName;
+		std::string nickName;
+		std::string phoneNumber;
+		std::string darkestSecret;
+
+	public:
+
+};
+
+#endif
